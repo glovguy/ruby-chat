c21d33602c9c759865a97e1ad4ecf23a6102c06d
Karl <karlsmith@bouzou.com>
2017-03-28 23:44:49 -0400

    Adds api endpoint for messages

--- app/controllers/chat_client_controller.rb
+++ app/controllers/chat_client_controller.rb
@@ -1,7 +1,13 @@
 class ChatClientController < ApplicationController
   layout 'ng_inject'
 
-  def index
+  def client
+    render 'chat_client/index.html.haml'
+  end
+
+  def messages
+    messages = Message.all
+    render json: messages
   end
 
 end
