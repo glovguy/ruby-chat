1d01a9a5ad9e7d2056d129f7407000eabef10032
Karl <karlsmith@bouzou.com>
2017-05-10 22:42:01 -0400

    Removes books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508022627) do
-
-  create_table "books", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170511024112) do
 
   create_table "chat_bots", force: :cascade do |t|
     t.string   "name"
