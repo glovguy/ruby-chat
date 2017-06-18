64d3352c8f46883166e083311d9f8497e9ee530a
Karl <karlsmith@bouzou.com>
2017-06-18 18:27:04 -0400

    Changes routes for messages api endpoint

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  class MessagesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+      messages = Messages.all
+      render json: messages, each_serializer: MessagesSerializer
+    end
+
+    def create
+      room = ChatRoom.first
+      room.new_message(message_attributes)
+      render json: { 'success' => 'message sent' }
+    end
+
+    private
+
+    def message_params
+      params.require(:data).permit(
+        attributes: [:body, :style, :sender]
+        )
+    end
+
+    def message_attributes
+      attributes = { 'sender' => 1, 'style' => 'user' }
+      attributes = attributes.merge(message_params[:attributes] || {})
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :users
-  resources :chat_bots
+  resources :chat_rooms
   get '/chat', to: 'chat_client#show'
-  scope :chat do
+  namespace :api do
     resources :messages, only: [:index, :create]
   end
+  get '/chat/messages', to: redirect('/api/messages')
+  post '/chat/messages', to: redirect('/api/messages')
+
   mount ActionCable.server => '/cable'
 end
