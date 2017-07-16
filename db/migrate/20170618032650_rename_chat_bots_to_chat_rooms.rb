1d245ed1270ec6409e1e2af21e4676616236f848
Karl <karlsmith@bouzou.com>
2017-07-15 23:04:24 -0400

    postgres

--- db/migrate/20170618032650_rename_chat_bots_to_chat_rooms.rb
+++ db/migrate/20170618032650_rename_chat_bots_to_chat_rooms.rb
@@ -2,7 +2,7 @@ class RenameChatBotsToChatRooms < ActiveRecord::Migration[5.0]
   def change
     rename_table :chat_bots, :chat_rooms
     add_reference :chat_rooms, :users, index: true
-    remove_reference :user, :chat_rooms
-    remove_reference :chat_rooms, :user
+    remove_reference :users, :chat_rooms
+    # remove_reference :chat_rooms, :user
   end
 end
