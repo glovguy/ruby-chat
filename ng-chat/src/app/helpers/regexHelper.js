9ab9ff0c93c8d438019fa85ba8dc5d0c8cdb0865
Karl <karlsmith@bouzou.com>
2017-07-21 09:56:13 -0400

    Renames ng-chat folder

--- ng-chat/src/app/helpers/regexHelper.js
+++
@@ -1,9 +0,0 @@
-let matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
-
-export function escapeStringRegexp (str) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
-  }
-
-  return str.replace(matchOperatorsRe, '\\$&');
-};
