c2239abfbd1be18f058bea70362d07b1f1e40e86
Karl <karlsmith@bouzou.com>
2017-07-23 13:43:34 -0400

    Adds setting to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RubyChat
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  config.serve_static_assets = true
   config.assets.paths << Rails.root.join('ng-chat/node_modules')
   config.assets.paths << Rails.root.join('ng-chat/dist')
   end
