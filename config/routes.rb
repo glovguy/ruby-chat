5ca3e1d90705a5d82f07e49c3ce5547d205a2a22
Karl <karlsmith@bouzou.com>
2017-12-24 23:34:29 -0600

    Adds redirect at root to /chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :chat_rooms
+  get '/', to: redirect('/chat')
   get '/chat', to: 'chat_client#show'
   namespace :api do
     resources :messages, only: [:index, :create]
